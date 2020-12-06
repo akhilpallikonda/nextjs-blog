0636deab5cf0539ba27b4362c3b18543e91c2b7c
Akhil Kumar Pallikonda <v-akpall@microsoft.com>
2020-12-06 22:55:45 +0530

    implemented prerendering,dynamic routes

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,43 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout/layout'
-import styles from '../components/common/styles/utils.module.css'
+import utilStyles from '../components/common/styles/utils.module.css'
+import { getSortedPostsData } from '../lib/posts'
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
+export default function Home({ allPostsData }) 
+  {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <section className={styles.headingMd}>
+      <section className={utilStyles.headingMd}>
         <p>[Your Self Introduction]</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   )
